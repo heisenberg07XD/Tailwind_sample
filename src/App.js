58160a42d1e793e616ad3f89e99fa9c5fd1d1655
Sudheep Aavula <sudheep@Sudheeps-MacBook-Pro.local>
2024-10-14 20:01:24 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,6 @@ import Example from "./components/home";
 export default function App() {
   return (
     // <Example/>
-    <h1 className="bg-green-400">sadf</h1>
+    <h1 className="bg-green-400">change</h1>
   )
 }
